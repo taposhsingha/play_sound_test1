cfb1066c2feab1f0a64633c248ec2be9f0f8f69a
taposhsingha <57071916+taposhsingha@users.noreply.github.com>
2023-05-29 14:38:46 +0600

    sound->readData and fstream codes added

---
+++ app/build/generated/source/buildConfig/debug/org/fmod/example/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.fmod.example;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.fmod.example";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: debug
+  public static final String[] FMOD_LIBS = { "fmodL" };
+}
